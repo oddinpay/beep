0ba48957a1e6a7826b46b872e78350c1337d456a
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-09-18 13:34:23 +0800

    Refactor Svelte config to include status handling and update preprocessors; add initial _status.svelte component

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,30 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import path from "node:path";
+
+const dirname = path.resolve(fileURLToPath(import.meta.url), "../");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	},
-	extensions: ['.svelte', '.svx']
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extensions: [".svx", ".md"],
+      status: {
+        faq: path.join(dirname, "./src/lib/componets/layout/_status.svelte"),
+      },
+    }),
+  ],
+  kit: {
+    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
+    adapter: adapter(),
+  },
+  extensions: [".svelte", ".svx", ".md"],
 };
 
 export default config;
