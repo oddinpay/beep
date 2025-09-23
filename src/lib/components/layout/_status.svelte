dc62bce8ba162e12e88ade35597afa41b287932c
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-09-24 02:19:55 +0800

    fix: Update API 2 status to 'up' and adjust overall status check logic

--- src/lib/components/layout/_status.svelte
+++ src/lib/components/layout/_status.svelte
@@ -37,7 +37,7 @@
 
   const apiDates = ["21/09/2025", "22/09/2025", "23/09/2025"];
 
-  const apiStatus = ["up", "down", "warn"] as StatusType[];
+  const apiStatus = ["up", "up", "warn"] as StatusType[];
 
 
   const mockData: ApiData[] = apiNames.map((name) => ({
@@ -773,7 +773,7 @@
                                 d="M10 2c4.42 0 8 3.58 8 8s-3.58 8-8 8s-8-3.58-8-8s3.58-8 8-8m1.13 9.38l.35-6.46H8.52l.35 6.46zm-.09 3.36c.24-.23.37-.55.37-.96c0-.42-.12-.74-.36-.97s-.59-.35-1.06-.35s-.82.12-1.07.35s-.37.55-.37.97c0 .41.13.73.38.96c.26.23.61.34 1.06.34s.8-.11 1.05-.34"
                               />
                             </svg>
-                          {:else if overallStatus === 'down'}
+                          {:else if api.statuses.some(s => s.status === 'down')}
                               <svg
                                   class="w-5 h-5 inline-block"
                                   xmlns="http://www.w3.org/2000/svg"
