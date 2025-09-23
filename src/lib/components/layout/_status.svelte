700f95306a44d2ead694076228e4377941d0e97d
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-09-24 02:06:07 +0800

    fix: Update API 2 status in example usage and enhance status display with icons

--- src/lib/components/layout/_status.svelte
+++ src/lib/components/layout/_status.svelte
@@ -85,7 +85,7 @@
 
   // Example usage of the function
   updateApiStatus("API 1", "21/09/2025", "up");
-  updateApiStatus("API 1", "22/09/2025", "up");
+  updateApiStatus("API 2", "22/09/2025", "up");
 
 
   let overallStatus = $derived.by(() => {
@@ -742,7 +742,45 @@
                 {#each mockData as api, index}
                   <section class="card" style="margin-bottom: 2px;">
                     <div class="card-header">
-                      <div>{api.name}</div>
+                      <div style="display: flex; align-items: center; gap: 5px;">
+                        {#if overallStatus === 'up'}
+                            <svg
+                              class="w-5 h-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                fill="#21ba45"
+                                fill-rule="evenodd"
+                                d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10m-5.97-3.03a.75.75 0 0 1 0 1.06l-5 5a.75.75 0 0 1-1.06 0l-2-2a.75.75 0 1 1 1.06-1.06l1.47 1.47l2.235-2.235L14.97 8.97a.75.75 0 0 1 1.06 0"
+                                clip-rule="evenodd"
+                              />
+                            </svg>
+                          {:else if overallStatus === 'warn'}
+                            <svg
+                              class="w-5 h-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                            >
+                              <path
+                                fill="#d97706"
+                                d="M10 2c4.42 0 8 3.58 8 8s-3.58 8-8 8s-8-3.58-8-8s3.58-8 8-8m1.13 9.38l.35-6.46H8.52l.35 6.46zm-.09 3.36c.24-.23.37-.55.37-.96c0-.42-.12-.74-.36-.97s-.59-.35-1.06-.35s-.82.12-1.07.35s-.37.55-.37.97c0 .41.13.73.38.96c.26.23.61.34 1.06.34s.8-.11 1.05-.34"
+                              />
+                            </svg>
+                          {:else if overallStatus === 'down'}
+                              <svg
+                                  class="w-5 h-5"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  viewBox="0 0 24 24"
+                                >
+                                <path
+                                  fill="#db2828"
+                                  d="M12 2c5.53 0 10 4.47 10 10s-4.47 10-10 10S2 17.53 2 12S6.47 2 12 2m3.59 5L12 10.59L8.41 7L7 8.41L10.59 12L7 15.59L8.41 17L12 13.41L15.59 17L17 15.59L13.41 12L17 8.41z"
+                                />
+                              </svg>
+                          {/if}
+                        <div>{api.name}</div>
+                      </div>
                       <div class="uptimes">
                         <span class="uptime15">{api.uptime15}% uptime</span>
                         <span class="uptime30">{api.uptime30}% uptime</span>
