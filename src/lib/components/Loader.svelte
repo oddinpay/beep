be833a73a7918c0e3d95955d15a4d92d187df134
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-09-23 20:29:06 +0800

    Update loader spinner color to enhance visibility

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -19,7 +19,7 @@
   <div
     class="fixed inset-0 bg-[#DCD9E7] bg-opacity-50 backdrop-blur-sm flex justify-center pt-15 z-[9999]"
   >
-    <Diamonds size="60" color="#6573D8" unit="px" duration="1s" />
+    <Diamonds size="60" color="green" unit="px" duration="1s" />
   </div>
 {/if}
 
