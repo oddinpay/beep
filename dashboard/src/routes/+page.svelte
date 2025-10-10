b6218af7c940a44ce1f8b7ecd0ce312dc1bac436
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-11 06:23:39 +0800

    Add nested tabs with content for improved organization and navigation

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -79,7 +79,39 @@
 					class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 md:col-span-2 md:h-[calc(40dvh-3rem)] md:overflow-y-hidden"
 				>
 					<TabsContent value="tab-0" class="h-auto min-h-[300px]">
-						<p class="text-base text-gray-200">Create Status Page</p>
+						<Tabs value="tab-1" class="items-end">
+							<TabsList
+								class="h-auto gap-2 rounded-full border-b border-border bg-zinc-800 px-0 py-1 text-zinc-400"
+							>
+								<TabsTrigger
+									value="tab-1"
+									class="relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Tab 1
+								</TabsTrigger>
+								<TabsTrigger
+									value="tab-2"
+									class="relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Tab 2
+								</TabsTrigger>
+								<TabsTrigger
+									value="tab-3"
+									class="relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Tab 3
+								</TabsTrigger>
+							</TabsList>
+							<TabsContent value="tab-1">
+								<p class="p-4 text-center text-xs text-white">Content for Tab 1</p>
+							</TabsContent>
+							<TabsContent value="tab-2">
+								<p class="p-4 text-center text-xs text-white">Content for Tab 2</p>
+							</TabsContent>
+							<TabsContent value="tab-3">
+								<p class="p-4 text-center text-xs text-white">Content for Tab 3</p>
+							</TabsContent>
+						</Tabs>
 					</TabsContent>
 				</div>
 			</div>
