3fbaf46551a7476462018682599b97aa47c4c624
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-11 20:03:34 +0800

    Refactor tab structure and content for Alerts and Incidents pages to improve organization and clarity

--- dashboard/src/routes/incidents/+page.svelte
+++ dashboard/src/routes/incidents/+page.svelte
@@ -69,38 +69,42 @@
 								class="h-auto gap-2 rounded-full border-b border-border bg-zinc-800 px-10 py-2 text-zinc-400"
 							>
 								<TabsTrigger
-									value="tab-1"
+									value="tab-2"
 									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
 								>
 									Create Incident
 								</TabsTrigger>
 								<TabsTrigger
-									value="tab-2"
+									value="tab-3"
 									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
 								>
 									Active
 								</TabsTrigger>
 								<TabsTrigger
-									value="tab-3"
+									value="tab-4"
 									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
 								>
 								 Pending	
 								</TabsTrigger>
 								<TabsTrigger
-									value="tab-4"
+									value="tab-5"
 									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
 								>
 									Resolved
 								</TabsTrigger>
 							</TabsList>
-							<TabsContent value="tab-1">
-								<p class="p-4 text-center text-xs text-white">No status page created</p>
-							</TabsContent>
 							<TabsContent value="tab-2">
-								<p class="p-4 text-center text-xs text-white">Content for Tab 2</p>
+								<p class="p-4 text-center text-xs text-white">No status page created</p>
 							</TabsContent>
 							<TabsContent value="tab-3">
+								<p class="p-4 text-center text-xs text-white">Content for Tab 2</p>
+							</TabsContent>
+							<TabsContent value="tab-4">
 								<p class="p-4 text-center text-xs text-white">No status page created</p>
+							</TabsContent>
+
+							<TabsContent value="tab-5">
+								<p class="p-4 text-center text-xs text-white">No status page </p>
 							</TabsContent>
 
 						</Tabs>
