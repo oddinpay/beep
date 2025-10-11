61c3830b1cf48c3ea0a8d189f1762424bee11c75
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-11 08:13:04 +0800

    Update tab trigger styles for improved consistency and adjust content layout for better organization

--- dashboard/src/routes/monitors/+page.svelte
+++ dashboard/src/routes/monitors/+page.svelte
@@ -21,7 +21,7 @@
 				<TabsTrigger
 					value="tab-0"
 					onclick={() => goto('/')}
-					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white"
+					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white xl:text-3xl"
 				>
 					<House class="h-8 w-8" /> Home
 				</TabsTrigger>
@@ -29,7 +29,7 @@
 				<TabsTrigger
 					value="tab-1"
 					onclick={() => goto('/monitors')}
-					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white"
+					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white xl:text-3xl"
 				>
 					<SquareActivity class="h-8 w-8" /> Monitors
 				</TabsTrigger>
@@ -37,7 +37,7 @@
 				<TabsTrigger
 					value="tab-2"
 					onclick={() => goto('/incidents')}
-					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white"
+					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white xl:text-3xl"
 				>
 					<ShieldAlert class="h-8 w-8" /> Incidents
 				</TabsTrigger>
@@ -45,7 +45,7 @@
 				<TabsTrigger
 					value="tab-3"
 					onclick={() => goto('/alerts')}
-					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white"
+					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white xl:text-3xl"
 				>
 					<Siren class="h-8 w-8" /> Alerts
 				</TabsTrigger>
@@ -53,7 +53,7 @@
 				<TabsTrigger
 					value="tab-4"
 					onclick={() => goto('/schedules')}
-					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white"
+					class="flex w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white xl:text-3xl"
 				>
 					<CalendarCheck class="h-8 w-8" /> Schedules
 				</TabsTrigger>
@@ -61,42 +61,48 @@
 
 			<div class="grid w-[1100px] grid-cols-1 gap-4 px-10 md:grid-cols-2">
 				<div
-					class="col-span-1 rounded-lg border border-border bg-zinc-900 p-8 md:col-span-2 md:h-[calc(40dvh-3rem)] md:overflow-y-hidden"
+					class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 md:col-span-2 md:h-[calc(40dvh-3rem)] md:overflow-y-hidden"
 				>
-					<TabsContent value="tab-0" class="h-auto min-h-[300px]">
-						<p class="text-base text-gray-200">Content for Tab 0</p>
+					<TabsContent value="tab-1" class="h-auto min-h-[300px]">
+						<Tabs value="tab-1" class=" items-center">
+							<TabsList
+								class="h-auto gap-2 rounded-full border-b border-border bg-zinc-800 px-10 py-2 text-zinc-400"
+							>
+								<TabsTrigger
+									value="tab-1"
+									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Active
+								</TabsTrigger>
+								<TabsTrigger
+									value="tab-2"
+									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Add New Monitor
+								</TabsTrigger>
+							</TabsList>
+							<TabsContent value="tab-1">
+								<p class="p-4 text-center text-xs text-white">No status page created</p>
+							</TabsContent>
+							<TabsContent value="tab-2">
+								<p class="p-4 text-center text-xs text-white">Content for Tab 2</p>
+							</TabsContent>
+						</Tabs>
+					</TabsContent>
+				</div>
+				<div
+					class="min-h-[300px] rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
+				>
+					<TabsContent value="tab-1" class="h-auto min-h-[150px]">
+						<p class="text-base text-gray-200">Up</p>
 					</TabsContent>
 				</div>
 
 				<div
-					class="min-h-[200px] rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
+					class="min-h-[300px] rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
 				>
-					<TabsContent value="tab-0" class="h-auto min-h-[150px]">
-						<p class="text-base text-gray-200">Content for Tab 1</p>
-					</TabsContent>
-				</div>
-
-				<div
-					class="min-h-[200px] rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
-				>
-					<TabsContent value="tab-0" class="h-auto min-h-[150px]">
-						<p class="text-base text-gray-200">Content for Tab 2</p>
-					</TabsContent>
-				</div>
-
-				<div
-					class="min-h-[200px] rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
-				>
-					<TabsContent value="tab-0" class="h-auto min-h-[150px]">
-						<p class="text-base text-gray-200">Content for Tab 3</p>
-					</TabsContent>
-				</div>
-
-				<div
-					class="min-h-[200px] rounded-lg border border-border bg-zinc-900 p-8 md:overflow-y-hidden"
-				>
-					<TabsContent value="tab-0" class="h-auto min-h-[150px]">
-						<p class="text-base text-gray-200">Content for Tab 4</p>
+					<TabsContent value="tab-1" class="h-auto min-h-[150px]">
+						<p class="text-base text-gray-200">Down</p>
 					</TabsContent>
 				</div>
 			</div>
