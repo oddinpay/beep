0765a5b7e34f24fac3d857d4f93f03703a6f7663
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-12 16:49:39 +0800

    fix: update dialog content background color and improve header text for clarity

--- dashboard/src/lib/components/Dialog.svelte
+++ dashboard/src/lib/components/Dialog.svelte
@@ -26,28 +26,36 @@
 				<Dialog.Trigger class={cn('cursor-pointer', buttonVariants({ variant: 'outline' }))}
 					>Create Status Page</Dialog.Trigger
 				>
-				<Dialog.Content class="bg-zinc-800">
+				<Dialog.Content class="bg-zinc-900">
+
 					<div class="flex flex-col items-center gap-2">
 						<div
 							class="flex size-11 shrink-0 items-center justify-center rounded-full border border-border"
 							aria-hidden="true"
 						>
 							<svg
-								class="size-6 stroke-svelte"
 								xmlns="http://www.w3.org/2000/svg"
-								width="28"
-								height="28"
-								viewBox="0 0 32 32"
 								aria-hidden="true"
+								width="28"
+								class="size-6"
+								height="28"
+								viewBox="0 0 512 512"
+								><path
+									fill="white"
+									d="m39.93 149.25l197.4 95.32c5.14 2.45 12 3.73 18.79 3.73s13.65-1.28 18.78-3.73l197.4-95.32c10.38-5 10.38-13.18 0-18.2L274.9 35.73c-5.13-2.45-12-3.73-18.78-3.73s-13.65 1.28-18.79 3.73l-197.4 95.32c-10.38 5.02-10.38 13.18 0 18.2"
+								/><path
+									fill="white"
+									d="M472.3 246.9s-36.05-17.38-40.83-19.72s-6.07-2.21-11.09.12s-145.6 70.23-145.6 70.23a45.7 45.7 0 0 1-18.78 3.74c-6.77 0-13.65-1.29-18.78-3.74c0 0-136.85-66-143.27-69.18C87 225 85 225 78.67 228l-39 18.78c-10.38 5-10.38 13.19 0 18.2L237.1 360.3c5.13 2.45 12 3.73 18.78 3.73s13.65-1.28 18.79-3.73l197.4-95.3c10.61-4.92 10.61-13.08.23-18.1"
+								/><path
+									fill="white"
+									d="M472.3 362.75s-36.05-17.38-40.83-19.75s-6.07-2.21-11.09.12S274.9 413.5 274.9 413.5a45.7 45.7 0 0 1-18.78 3.73c-6.77 0-13.65-1.28-18.79-3.73c0 0-136.85-66-143.26-69.18c-7-3.39-9-3.39-15.29-.35l-39 18.78c-10.39 5-10.39 13.18 0 18.2l197.4 95.32c5.13 2.56 12 3.73 18.78 3.73s13.65-1.28 18.78-3.73L472.18 381c10.5-5.07 10.5-13.23.12-18.25"
+								/></svg
 							>
-								<circle cx="16" cy="16" r="13" fill="none" stroke-width="2" />
-								<circle cx="16" cy="16" r="9" fill="none" stroke-width="2" />
-							</svg>
 						</div>
 						<Dialog.Header>
-							<Dialog.Title class="sm:text-center">Sign up Origin UI</Dialog.Title>
-							<Dialog.Description class="sm:text-center">
-								We just need a few details to get you started.
+							<Dialog.Title class="text-gray-300 sm:text-center">New Status Page</Dialog.Title>
+							<Dialog.Description class="text-gray-400 sm:text-center">
+								Set up and publish your status page.
 							</Dialog.Description>
 						</Dialog.Header>
 					</div>
@@ -72,21 +80,13 @@
 								/>
 							</div>
 						</div>
-						<Button type="button" class="w-full">Sign up</Button>
 					</form>
 
 					<div
 						class="flex items-center gap-3 before:h-px before:flex-1 before:bg-border after:h-px after:flex-1 after:bg-border"
-					>
-						<span class="text-xs text-muted-foreground">Or</span>
-					</div>
+					></div>
 
-					<Button variant="outline">Continue with Google</Button>
-
-					<p class="text-center text-xs text-muted-foreground">
-						By signing up you agree to our
-						<a class="underline hover:no-underline" href="#title">Terms</a>.
-					</p>
+					<Button variant="outline">Publish Page</Button>
 				</Dialog.Content>
 			</Dialog.Root>
 		</div>
