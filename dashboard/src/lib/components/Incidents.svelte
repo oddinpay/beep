0da5f98e6c64da9f1dd839a7e99f74309978dea1
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-13 19:41:46 +0800

    fix: update label from 'Monitor Type' to 'Status' for clarity

--- dashboard/src/lib/components/Incidents.svelte
+++ dashboard/src/lib/components/Incidents.svelte
@@ -97,7 +97,7 @@
 								/>
 							</div>
 							<div class="space-y-2">
-								<Label for="{uid}-title" class="font-bold text-gray-300">Monitor Type</Label>
+								<Label for="{uid}-title" class="font-bold text-gray-300">Status</Label>
 								<Select.Root type="single" bind:value>
 									<Select.Trigger
 										id={uid}
