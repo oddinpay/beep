0c683b3eada23f1a80a73cf3cfb56f302570cf9d
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-14 19:06:11 +0800

    feat: wrap Connect component in Empty component for improved structure

--- dashboard/src/lib/components/Connect.svelte
+++ dashboard/src/lib/components/Connect.svelte
@@ -1,23 +1,31 @@
 <script lang="ts">
 	import Input from '$lib/components/ui/input.svelte';
 	import Label from '$lib/components/ui/label.svelte';
+	import * as Empty from '$lib/components/ui/empty/index.js';
+	import IconFileOrientation from '@tabler/icons-svelte/icons/file-orientation';
 
 	const uid = $props.id();
 </script>
 
-<div class="*:not-first:mt-2">
-	<Label class="text-gray-300" for={uid}>Connect to API</Label>
-	<div class="flex rounded-lg shadow-xs shadow-black/5">
-		<span
-			class="inline-flex items-center rounded-s-lg border border-zinc-700 bg-zinc-900 px-3 text-sm text-muted-foreground"
-		>
-			https://
-		</span>
-		<Input
-			id={uid}
-			class="-ms-px rounded-s-none border-zinc-700 text-white shadow-none"
-			placeholder="Hostname"
-			type="text"
-		/>
-	</div>
-</div>
+<Empty.Root>
+	<Empty.Header>
+		<Empty.Description class="text-gray-400">
+			<div class=" *:not-first:mt-2">
+				<Label class="text-gray-300" for={uid}>Connect API Instance</Label>
+				<div class="flex rounded-lg shadow-xs shadow-black/5">
+					<span
+						class="inline-flex items-center rounded-s-lg border border-zinc-700 bg-zinc-900 px-3 text-sm text-muted-foreground"
+					>
+						https://
+					</span>
+					<Input
+						id={uid}
+						class="-ms-px rounded-s-none border-zinc-700 text-white shadow-none"
+						placeholder="Hostname"
+						type="text"
+					/>
+				</div>
+			</div>
+		</Empty.Description>
+	</Empty.Header>
+</Empty.Root>
