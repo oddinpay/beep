0eeb882d7500c5e81d33ce9f17445b5952608dfb
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-14 22:04:30 +0800

    fix: enhance focus styles for dropdown menu items for better accessibility

--- dashboard/src/lib/components/_extras/navbars/user-menu.svelte
+++ dashboard/src/lib/components/_extras/navbars/user-menu.svelte
@@ -40,18 +40,21 @@
 		</DropdownMenuLabel>
 		<DropdownMenuSeparator class="bg-zinc-600" />
 		<DropdownMenuGroup>
-			<DropdownMenuItem onclick={() => goto('/connect')} class="cursor-pointer text-zinc-100">
+			<DropdownMenuItem
+				onclick={() => goto('/connect')}
+				class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white"
+			>
 				<Unplug size={16} class="opacity-60" aria-hidden="true" />
 				<span>Connect</span>
 			</DropdownMenuItem>
 			<DropdownMenuSeparator class="bg-zinc-600" />
-			<DropdownMenuItem class="cursor-pointer text-zinc-100">
+			<DropdownMenuItem class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white">
 				<BookOpenText size={16} class="opacity-60" aria-hidden="true" />
 				<span>Docs</span>
 			</DropdownMenuItem>
 		</DropdownMenuGroup>
 		<DropdownMenuSeparator class="bg-zinc-600" />
-		<DropdownMenuItem class="cursor-pointer text-zinc-100">
+		<DropdownMenuItem class="cursor-pointer text-zinc-100 focus:bg-zinc-600 focus:text-white">
 			<LogOutIcon size={16} class="opacity-60" aria-hidden="true" />
 			<span>Sign out</span>
 		</DropdownMenuItem>
