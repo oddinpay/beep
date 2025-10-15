6f705a43dcf3b07fd3495bb19d50f7808a9432d6
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-15 20:46:44 +0800

    fix: update status handling logic to use overallStatus for rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -279,7 +279,7 @@
     
     if (browser && allStatuses.length === 0) {
       const stored = statusStore.get();
-      statusStore.set([]);
+      // statusStore.set([]);
       return stored ? stored[0] : undefined;
     }
 
@@ -290,7 +290,7 @@
 
     
     if (computed && currentStoredStatus !== computed) {
-      statusStore.set([computed]);
+      // statusStore.set([computed]);
       return computed;
     }
 
@@ -1142,7 +1142,7 @@
                               {#each monitors as status}
                                 <div class="status-card">
                                   <div style="display: flex; align-items: center; gap: 10px;">
-                                    {#if status.status === 'up'}
+                                    {#if overallStatus === 'up'}
                                       <svg
                                         class="w-10 h-10 inline-block"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -1155,7 +1155,7 @@
                                           clip-rule="evenodd"
                                         />
                                       </svg>
-                                    {:else if status.status === 'warn'}
+                                    {:else if overallStatus === 'warn'}
                                       <svg
                                         class="w-10 h-10 inline-block"
                                         xmlns="http://www.w3.org/2000/svg"
@@ -1166,7 +1166,7 @@
                                           d="M10 2c4.42 0 8 3.58 8 8s-3.58 8-8 8s-8-3.58-8-8s3.58-8 8-8m1.13 9.38l.35-6.46H8.52l.35 6.46zm-.09 3.36c.24-.23.37-.55.37-.96c0-.42-.12-.74-.36-.97s-.59-.35-1.06-.35s-.82.12-1.07.35s-.37.55-.37.97c0 .41.13.73.38.96c.26.23.61.34 1.06.34s.8-.11 1.05-.34"
                                         />
                                       </svg>
-                                    {:else if status.status === 'down'}
+                                    {:else if overallStatus === 'down'}
                                         <svg
                                           class="w-10 h-10 inline-block"
                                           xmlns="http://www.w3.org/2000/svg"
