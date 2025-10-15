f2359d264f0ccc794bf87f75c15c334a6435f460
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-15 17:23:38 +0800

    fix: update minimum height for layout consistency in schedules/+page.svelte

--- dashboard/src/routes/schedules/+page.svelte
+++ dashboard/src/routes/schedules/+page.svelte
@@ -62,7 +62,7 @@
 
 			<div class="grid w-full grid-cols-1 gap-4 overflow-y-auto px-10 pb-20 md:grid-cols-2">
 				<div
-					class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 md:col-span-2 md:h-[calc(50dvh-3rem)] md:overflow-y-hidden"
+					class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 md:col-span-2 md:min-h-[480px] md:overflow-y-hidden"
 				>
 					<TabsContent value="tab-4" class="h-auto min-h-[300px]">
 						<Tabs value="tab-4" class=" items-center">
