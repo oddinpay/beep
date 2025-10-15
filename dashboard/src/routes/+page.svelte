6d942f2424f16ec07113248a294bb7a4eb6c9d7b
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-15 11:11:12 +0800

    fix: remove unnecessary whitespace around Header component in +page.svelte

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -12,9 +12,7 @@
 <div class="flex min-h-screen items-start overflow-hidden justify-center  bg-black">
 	<div class=" mx-auto fixed px-4 py-4 w-full max-w-7xl">
 		<div class="px-4 py-4">
-
 			<Header />
-
 		</div>
 
 		<Tabs value={currentTab} class="flex w-full flex-col max-h-[calc(100dvh-3rem)] gap-8 md:flex-row">
