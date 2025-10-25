a7d52e1dc4ccd4b50781575fe7c4848cb41602e8
0x64 <senal666@icloud.com>
2025-10-25 20:13:12 +0800

    Add CORS header to allow cross-origin requests in RestRequestHandler

--- api/main.go
+++ api/main.go
@@ -1101,6 +1101,7 @@ func RestRequestHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", StatusMethodNotAllowed)
 	}
 
+	w.Header().Set(HeaderAllowOrigin, "*")
 	w.Header().Set(HeaderContentType, ContentTypeJSON)
 
 	var hasMonitors bool
