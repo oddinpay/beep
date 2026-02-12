08ddcf72234a4cdb0ed53befd5f2956845c834e1
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-02-12 22:16:23 +0900

    Update fetch request method to POST for status endpoint

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,11 @@ import { env } from "../env/server";
 
 export const load: PageServerLoad = async () => {
   const data = async () => {
-    return (await fetch(`https://${env.BEEP_HOST}/v1/status`)).json();
+    return (
+      await fetch(`https://${env.BEEP_HOST}/v1/status`, {
+        method: "POST",
+      })
+    ).json();
   };
 
   return data();
