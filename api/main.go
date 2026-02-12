cddf5b35c70c703dd567c18b6e6472506996a391
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-02-12 16:51:16 +0900

    Reduce channel buffer size for clientChan in Sse handler to improve resource management

--- api/main.go
+++ api/main.go
@@ -605,7 +605,7 @@ func Sse(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
-	clientChan := make(chan map[string]StatusPayload, 50)
+	clientChan := make(chan map[string]StatusPayload, 1)
 
 	globalHub.Lock()
 	globalHub.clients[clientChan] = struct{}{}
