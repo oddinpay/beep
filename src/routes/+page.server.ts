314cfebbc97dea99cbd30f72f4324b609a668bd5
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-02-12 22:20:11 +0900

    Revert fetch request method to GET for status endpoint

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,11 +3,7 @@ import { env } from "../env/server";
 
 export const load: PageServerLoad = async () => {
   const data = async () => {
-    return (
-      await fetch(`https://${env.BEEP_HOST}/v1/status`, {
-        method: "POST",
-      })
-    ).json();
+    return (await fetch(`https://${env.BEEP_HOST}/v1/status`)).json();
   };
 
   return data();
