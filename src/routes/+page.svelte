387c2415ead797063d4fc27b3759d45cf4bffa32
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-02-24 18:05:45 +0900

    Replace Loader component with animated SVG for improved loading indication

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  // import { localStore } from '$lib/storage';
   import { Button } from "$lib/components/ui/button/index.js";
   import Buttong from "$lib/components/Buttong.svelte";
   import Footer from "$lib/components/Footer.svelte";
@@ -14,8 +13,6 @@
   import { browser } from "$app/environment";
   import timer from "$lib/timer";
   import { env } from "$env/dynamic/public";
-  import Loader from "$lib/components/Loader.svelte";
-
   let signin = "https://oddinpay.com/signin";
   let signup = "https://oddinpay.com/signup";
   let slug = "https://oddinpay.com";
@@ -999,7 +996,43 @@
                         {:else if overallStatus === "down"}
                           Critical Issues Detected
                         {:else}
-                          <Loader />
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="48"
+                            height="48"
+                            viewBox="0 0 24 24"
+                            ><circle cx="18" cy="12" r="0" fill="currentColor"
+                              ><animate
+                                attributeName="r"
+                                begin=".67"
+                                calcMode="spline"
+                                dur="1.5s"
+                                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                                repeatCount="indefinite"
+                                values="0;2;0;0"
+                              /></circle
+                            ><circle cx="12" cy="12" r="0" fill="currentColor"
+                              ><animate
+                                attributeName="r"
+                                begin=".33"
+                                calcMode="spline"
+                                dur="1.5s"
+                                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                                repeatCount="indefinite"
+                                values="0;2;0;0"
+                              /></circle
+                            ><circle cx="6" cy="12" r="0" fill="currentColor"
+                              ><animate
+                                attributeName="r"
+                                begin="0"
+                                calcMode="spline"
+                                dur="1.5s"
+                                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                                repeatCount="indefinite"
+                                values="0;2;0;0"
+                              /></circle
+                            ></svg
+                          >
                         {/if}
                       </h1>
                       <p
