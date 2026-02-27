67a1e2abd7b74f454e5c9746133144000ea496ec
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-02-28 07:49:34 +0900

    Update label for Subscribers tab in Alerts component

--- dashboard/src/routes/alerts/+page.svelte
+++ dashboard/src/routes/alerts/+page.svelte
@@ -79,7 +79,7 @@
 									value="tab-5"
 									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
 								>
-									Subscribers
+									Subscribers	
 								</TabsTrigger>
 								<TabsTrigger
 									value="tab-6"
