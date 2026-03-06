dcae664594ba4c33d62c529a2ec490ab5051abec
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-06 15:56:27 +0900

    Refactor status query handler to include argument parameter and improve error handling

--- dashboard/src/convex/status.ts
+++ dashboard/src/convex/status.ts
@@ -2,8 +2,12 @@ import { query } from './_generated/server';
 
 export const get = query({
 	args: {},
-	handler: async (ctx) => {
+	handler: async (ctx, args) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (identity === null) {
+			throw new Error('Unauthenticated call to mutation');
+		}
 		const status = await ctx.db.query('status').collect();
-		return status.map((status) => ({ ...status, assigner: 'oddin'}));
+		return status.map((status) => ({ ...status, assigner: 'oddin' }));
 	}
 });
